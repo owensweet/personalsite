a36beac8189e340886dff6d66d2d2fc8614243e4
owensweet <o.sweet.work@gmail.com>
2024-10-01 20:15:28 -0700

    Second commit, initializing folders and node express server

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+const PORT = 8000;
+
+//Set view engine
+app.set('view engine', 'ejs');
+
+//Create virtual paths using static
+app.use("/js", express.static("./public/js"));
+app.use("/css", express.static("./public/css"));
+app.use("/img", express.static("./public/img"));
+
+//Middleware so I can parse requests
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render("index");
+    return;
+})
+
+app.listen(PORT, () => {
+    console.log("Node application listening on port " + PORT);
+})
+
